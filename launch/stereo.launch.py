03ae47e11c6c350f93b68198f4a907feed2ef011
clandsmeer <clandsmeer@student.ethz.ch>
2025-11-24 10:38:06 +0000

    Configure launch for stereo inertial
    Also adapt CMakeLists to be compatible with the changes merged
    in from the stereo-inertial branch

--- launch/stereo.launch.py
+++ launch/stereo.launch.py
@@ -25,15 +25,15 @@ def generate_launch_description():
             namespace='ORB_SLAM3',
             output='screen',
             parameters=[
-                {'settings_file': package_dir + '/orb_slam3/config/Stereo/RealSense_D555.yaml'},
+                {'settings_file': package_dir + '/orb_slam3/config/Stereo/HalfTank_Easy.yaml'},
                 {'voc_file': package_dir + '/orb_slam3/Vocabulary/ORBvoc.txt.bin'},
-                {'img0_topic': '/cam_realsense/camera/infra1/image_rect_raw'},
-                {'img1_topic': '/cam_realsense/camera/infra2/image_rect_raw'},
-                {'imu_topic': '/cam_realsense/camera/imu'},
+                {'img0_topic': '/camera/left/image_dehazed/raw'},
+                {'img1_topic': '/camera/right/image_dehazed/raw'},
+                {'imu_topic': '/imu/data'},
                 {'enable_debug_window': True},
                 {'is_inertial': True},
                 {'manual_time_sync': True},
-                {'imu_from_yaml': False},
+                {'imu_from_yaml': True},
             ]
     )
 
