8f99104e3e7b158a5a625732e266472f4e70d46a
D-i3g0-H <diegoh0201@gmail.com>
2025-11-24 13:41:21 +0000

    launch

--- launch/stereo-sim.launch.py
+++ launch/stereo-sim.launch.py
@@ -32,7 +32,7 @@ def generate_launch_description():
                 {'img1_topic': '/camera_d455/ir_right/image_raw'},
                 {'imu_topic': '/vectornav/Imu_raw'},
                 {'enable_debug_window': True},
-                {'is_inertial': False},
+                {'is_inertial': True},
                 {'manual_time_sync': False},
                 {'imu_from_yaml': False},
             ]
