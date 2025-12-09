2f35cf053c025ae8eaf701dd692e60f622a39ce9
Paul Joseph <33182750+paulijosey@users.noreply.github.com>
2025-12-09 16:45:58 +0100

    Feature/dynamic init (#2)
    * frame id changes
    
    * publish inverse transform
    
    * stellar setup
    
    * stellar configs
    
    ---------
    
    Co-authored-by: Paul Joseph <paul@bubble-robotics.com>

--- launch/stereo-sim.launch.py
+++ launch/stereo-sim.launch.py
@@ -31,7 +31,7 @@ def generate_launch_description():
                 {'img0_topic': '/camera_d455/ir_left/image_raw'},
                 {'img1_topic': '/camera_d455/ir_right/image_raw'},
                 {'imu_topic': '/vectornav/Imu_raw'},
-                {'enable_debug_window': True},
+                {'enable_debug_window': False},
                 {'is_inertial': True},
                 {'manual_time_sync': False},
                 {'imu_from_yaml': False},
--- src/stereo.cpp
+++ src/stereo.cpp
@@ -378,7 +378,7 @@ void StereoMode::PublishOdometry(const Sophus::SE3f& Twc, const sensor_msgs::msg
     nav_msgs::msg::Odometry odom_msg;
     odom_msg.header.stamp = img_msg->header.stamp;
     odom_msg.header.frame_id = worldFrameId_;
-    odom_msg.child_frame_id = cameraFrameOrbId;
+    odom_msg.child_frame_id = cameraFrameId_;
     
     Eigen::Vector3f t = Twc.translation();
     Eigen::Quaternionf q = Twc.unit_quaternion();
@@ -423,11 +423,14 @@ void StereoMode::PublishTF(const Sophus::SE3f& Twc, const sensor_msgs::msg::Imag
 {
     geometry_msgs::msg::TransformStamped transform;
     transform.header.stamp = img_msg->header.stamp;
-    transform.header.frame_id = worldFrameId_;
-    transform.child_frame_id = cameraFrameOrbId;
+    transform.header.frame_id = cameraFrameId_;
+    transform.child_frame_id = worldFrameId_;
     
-    Eigen::Vector3f t = Twc.translation();
-    Eigen::Quaternionf q = Twc.unit_quaternion();
+    // use inverse of Twc for transform from world to camera
+    Sophus::SE3f Tcw = Twc.inverse();
+
+    Eigen::Vector3f t = Tcw.translation();
+    Eigen::Quaternionf q = Tcw.unit_quaternion();
     
     transform.transform.translation.x = t.x();
     transform.transform.translation.y = t.y();
@@ -440,6 +443,7 @@ void StereoMode::PublishTF(const Sophus::SE3f& Twc, const sensor_msgs::msg::Imag
     
     tfBroadcaster_->sendTransform(transform);
 }
+
 
 void StereoMode::PublishMapPoints(const std_msgs::msg::Header& header)
 {
