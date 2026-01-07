9901dba8ad6baaab3c1036ba094ae24caedbce1a
clandsmeer <clandsmeer@student.ethz.ch>
2026-01-07 21:28:41 +0000

    Feat: stereo-inertial works on tank here -- not sure why
    Same config and launch file as in the other branches I think

--- launch/stereo.launch.py
+++ launch/stereo.launch.py
@@ -18,26 +18,26 @@ def generate_launch_description():
         "ros2_orb_slam3")
 
     # Create nodes
-    stereo_node: Node = Node(
+    mono_inertial_node: Node = Node(
             package='ros2_orb_slam3',
             executable='stereo_node_cpp',
             name='stereo_node',
             namespace='ORB_SLAM3',
             output='screen',
             parameters=[
-                {'settings_file': package_dir + '/orb_slam3/config/Stereo/RealSense_D455.yaml'},
+                {'settings_file': package_dir + '/orb_slam3/config/Stereo/HalfTank_Easy.yaml'},
                 {'voc_file': package_dir + '/orb_slam3/Vocabulary/ORBvoc.txt.bin'},
-                {'img0_topic': '/cam_realsense/camera/infra1/image_rect_raw'},
-                {'img1_topic': '/cam_realsense/camera/infra2/image_rect_raw'},
-                {'imu_topic': '/cam_realsense/camera/imu'},
+                {'img0_topic': '/camera/left/image_dehazed/raw'},
+                {'img1_topic': '/camera/right/image_dehazed/raw'},
+                {'imu_topic': '/imu/data'},
                 {'enable_debug_window': True},
                 {'is_inertial': True},
                 {'manual_time_sync': False},
-                {'imu_from_yaml': False},
+                {'imu_from_yaml': True},
             ]
     )
 
     return LaunchDescription([
-        stereo_node,
+        mono_inertial_node,
     ])
 
