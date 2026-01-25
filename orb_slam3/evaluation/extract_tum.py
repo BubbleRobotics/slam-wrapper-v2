2331f3006f1ac9d375f3802dc186c1bb270b719e
D-i3g0-H <diegoh0201@gmail.com>
2026-01-25 22:27:19 +0000

    adding tests

--- orb_slam3/evaluation/extract_tum.py
+++ orb_slam3/evaluation/extract_tum.py
@@ -45,7 +45,7 @@ BAG_FILE = resolve_bag_path(args.mcap)
 print(f"Using bag file: {BAG_FILE}")
 
 TOPIC_GT = "/model/bluerov2/odometry"
-TOPIC_ODO = "/ORB_SLAM3/stereo_sim_node/odometry"
+TOPIC_ODO = "/ORB_SLAM3/mono_sim_node/odometry"
 
 OUT_GT = "ground_truth.txt"
 OUT_ODO = "odometry.txt"
