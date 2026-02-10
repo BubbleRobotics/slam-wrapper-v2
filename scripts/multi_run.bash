21f551d2d1ce3cde40511918438e960f02cf4706
Cedric Landsmeer <clandsmeer@student.ethz.ch>
2026-02-10 17:46:13 +0000

    feat: make multi eval path agnostic

--- scripts/multi_run.bash
+++ scripts/multi_run.bash
@@ -3,16 +3,30 @@
 # This script checks the file multi_run_list.txt in the same directory
 # For each of the sequences contained, it will make N tests
 
-# Notes:
+### ADAPT THESE PATHS ###
+
+# Set the path where the trajectories should be saved
+savedir="/home/semesterproject/data/runs"
+
+# Set the path to the directory of the dataset
+datadir="/home/semesterproject/data/tank"
+
+# Absolute path to the semesterprojec directory
+projectdir="/home/semesterproject"
+
+
+
 # - Make sure the paths to the rosbags and saving locations work out
 # - Make sure you use the right parameters and have the pipeline compiled
 #   as you want it before running this. Also: the convert_compressed_img node 
 #   has to be running in parallel in a separate terminal.
 # - The multi_run_list.txt file must not contain any additional whitespace 
 #   and end with a newline
-N=10
-factors=("1.0" "0.1" "0.01" "10.0")
+N=2
+factors=("1.0" "0.1")
 # factors=("10.0")
+
+srcdir="${projectdir}/src"
 
 for factor in "${factors[@]}"; do
 
@@ -23,7 +37,7 @@ for factor in "${factors[@]}"; do
         echo " -- SEQUENCE ${NAME} -- "
 
         # New directory if necessary
-        dirname="/home/ubuntu/ws_blue/data/runs/${NAME}/sidm_dvlcov${factor/./p}"
+        dirname="${savedir}/${NAME}/sidm_dvlcov${factor/./p}"
 
         if [ ! -d $dirname ]; then
             echo "New directory"
@@ -33,9 +47,9 @@ for factor in "${factors[@]}"; do
         fi
 
         # Make the changes and recompile
-        sed -i "8 s/=[^*]*/=${factor}/" /home/ubuntu/ws_blue/src/slam-wrapper-v2/orb_slam3/src/DvlTypes.cc
+        sed -i "8 s/=[^*]*/=${factor}/" "${srcdir}/slam-wrapper-v2/orb_slam3/src/DvlTypes.cc"
         
-        xterm -e bash -c "cd /home/ubuntu/ws_blue && pwd && colcon build" &
+        xterm -e bash -c "cd "${projectdir}" && pwd && colcon build" &
         BUILDER_PID=$!
         wait $BUILDER_PID
 
@@ -43,18 +57,18 @@ for factor in "${factors[@]}"; do
         do
 
             # Start recorder
-            xterm -e bash -c "source ~/ws_blue/install/setup.bash && ros2 run convert_compressed_img save_trajec" &
+            xterm -e bash -c "source ${projectdir}/install/setup.bash && ros2 run convert_compressed_img save_trajec" &
             RECORDER_XTERM_PID=$!
             echo "Recorder xterm PID: $RECORDER_XTERM_PID"
 
             # Start pipeline
-            xterm -e bash -c "source ~/ws_blue/install/setup.bash && ros2 launch ros2_orb_slam3 stereo.launch.py" &
+            xterm -e bash -c "source ${projectdir}/install/setup.bash && ros2 launch ros2_orb_slam3 stereo.launch.py" &
             PIPELINE_XTERM_PID=$!
             echo "Pipeline xterm PID: $PIPELINE_XTERM_PID"
             sleep 2  # give everything a moment to start up
 
             # Start rosbag
-            xterm -e bash -c "source ~/ws_blue/install/setup.bash && ros2 bag play /home/ubuntu/ws_blue/data/tank/${NAME}/" &
+            xterm -e bash -c "source ${projectdir}/install/setup.bash && ros2 bag play ${datadir}/${NAME}/" &
             PLAYER_XTERM_PID=$!
             echo "Player xterm PID: $PLAYER_XTERM_PID"
 
@@ -81,6 +95,6 @@ for factor in "${factors[@]}"; do
 
         done
 
-    done < /home/ubuntu/ws_blue/src/slam-wrapper-v2/scripts/multi_run_list.txt
+    done < "${projectdir}/src/slam-wrapper-v2/scripts/multi_run_list.txt"
 
 done
