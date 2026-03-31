7eb9926a34459455379edbaeac9dfc700d896cc4
Cedric Landsmeer <clandsmeer@student.ethz.ch>
2026-03-31 12:51:31 +0000

    feat: params from 45 degree Oak lake test:

--- launch/stereo.launch.py
+++ launch/stereo.launch.py
@@ -5,11 +5,12 @@
  # @ Description: Launch file for Monocular SLAM Wrapper with Realsense D555 Camera
  '''
 
+from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import DeclareLaunchArgument
 from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
-from ament_index_python.packages import get_package_share_directory
+
 
 def generate_launch_description():
 
@@ -25,13 +26,13 @@ def generate_launch_description():
             namespace='ORB_SLAM3',
             output='screen',
             parameters=[
-                {'settings_file': package_dir + '/orb_slam3/config/Stereo/paramsAquaSlam_WholeTank.yaml'},
+                {'settings_file': package_dir + '/orb_slam3/config/Stereo/paramsLake.yaml'},
                 {'voc_file': package_dir + '/orb_slam3/Vocabulary/ORBvoc.txt.bin'},
                 {'img0_topic': '/camera/left/image_dehazed/raw'},
                 {'img1_topic': '/camera/right/image_dehazed/raw'},
-                {'imu_topic': '/imu/data'},
+                {'imu_topic': '/vectornav/Imu_body'},
                 {'enable_debug_window': True},
-                {'is_inertial': True},
+                {'is_inertial': False},
                 {'manual_time_sync': False},
                 {'imu_from_yaml': True},
             ]
